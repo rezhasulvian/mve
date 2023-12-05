f26be74a885807ca339e947a9266f63702f17e40
Ridwanabout <plester1997@gmail.com>
2023-12-05 17:40:35 +0700

    Ridwanabout

---
+++ assets/js/app/dashboard.js
@@ -0,0 +1,480 @@
+/**
+ * Created by User on 31/08/2021.
+ */
+
+var kecSelector, kelSelector, kecVotSeletor, kelVotSelector;
+
+const primary = '#6993FF';
+const success = '#1BC5BD';
+const info = '#8950FC';
+const warning = '#FFA800';
+const danger = '#F64E60';
+const light = "#fff";
+
+function Dashboard() {
+    var self = this;
+
+    self.init = function() {
+
+        kecSelector = $('#sKecId').select2({
+            width: '100%',
+            placeholder: "Silahkan pilih Kecamatan"
+        });
+
+        kelSelector = $('#sKelId').select2({
+            width: '100%',
+            placeholder: "Silahkan pilih kelurahan"
+        });
+
+        // kecVotSelector = $('#sKecVotId').select2({
+        //     width: '100%',
+        //     placeholder: "Silahkan pilih Kecamatan"
+        // });
+
+        // kelVotSelector = $('#sKelVotId').select2({
+        //     width: '100%',
+        //     placeholder: "Silahkan pilih kelurahan"
+        // });
+
+        $('#sKelId').attr('disabled', true);
+        $('#sKelVotId').attr('disabled', true);
+
+
+        var date = new Date();
+
+        var dateName = moment().format("DD MMMM YYYY");
+        var monthName = moment().format("MMMM YYYY");
+        var timeName = moment().format("HH:mm");
+
+        pCurrDate = moment(date).format("dd MM yyyy");
+        pCurrMonth = moment(date).format("MM yyyy");
+
+
+        $('#sDate').datepicker({
+            format: 'dd MM yyyy',
+            todayHighlight: true,
+            autoclose: true,
+            orientation: 'auto bottom',
+        });
+
+        $('#sDate').datepicker({dateFormat: 'dd MM yyyy'}).datepicker("setDate", pCurrDate);
+
+        $('#dateInfo').text("DPRD Kabupaten / Kota" + " " + dateName + " Pukul " + timeName + " WIB");
+        $('#dateInfoV').text("DPRD Kabupaten / Kota" + " " + dateName + " Pukul " + timeName + " WIB");
+
+
+        // $('#sDate').flatpickr("defaultDate", pCurrDate);
+
+        loadVotingChart(44, 55, 13, 43, 22, 10, 50, 34, 25, 30, 35, 20);
+        loadKtpKec();
+        // loadKtpKel();
+
+
+        $('#sDate').on("change", function () {
+
+            loadVotingChart(55, 44, 10, 10, 22, 10, 50, 22, 25, 30, 25, 50);
+            loadKtpKec();
+            // loadKtpKel();
+
+            if ($('#sKecId').val() == "") {
+                $('#dateInfo').text("DPRD Kabupaten / Kota" + " " + this.value + " Pukul " + timeName + " WIB");
+                $('#dateInfoV').text("DPRD Kabupaten / Kota" + " " + this.value + " Pukul " + timeName + " WIB");
+            } else {
+                $('#dateInfo').text($('#sKecId').val() + " " + this.value + " Pukul " + timeName + " WIB");
+                $('#dateInfoV').text($('#sKecId').val() + " " + this.value + " Pukul " + timeName + " WIB");
+            }
+
+        });
+
+        $('#sKecId').change(function() {
+            $('#sKelId').attr('disabled', false);
+        });
+
+        // $('#sKecVotId').change(function() {
+        //     $('#sKelVotId').attr('disabled', false);
+        // });
+
+        $('#searchGroupingBtn').on('click', function(e) {
+            e.preventDefault();
+            if ($('#sKecId').val() == "" && $('#sKelId').val() == ""){
+                // loadKtpKec();
+                Swal.fire({
+                    position: "top-right",
+                    icon: "warning",
+                    title: "Pilih kecamatan terlebih dahulu!",
+                    showConfirmButton: false,
+                    timer: 1500
+                });
+                // console.log("kecamatan dan kelurahan kosong")
+            }
+            else if($('#sKelId').val() == "") {
+                console.log($('#sKecId').val())
+
+                $('#dateInfo').text($('#sKecId').val() + " " + dateName + " Pukul " + timeName + " WIB");
+                $('#dateInfoV').text($('#sKecId').val() + " " + dateName + " Pukul " + timeName + " WIB");
+
+                loadKtpKec();
+                // console.log("kecamatan isi dan kelurahan kosong")
+            }
+            else {
+                // loadKtpKel();
+
+                $('#dateInfo').text($('#sKecId').val() + " - " + $('#sKelId').val() + " " + dateName + " Pukul " + timeName + " WIB");
+                $('#dateInfoV').text($('#sKecId').val() + " - " + $('#sKelId').val() + " " + dateName + " Pukul " + timeName + " WIB");
+
+                console.log("Kecamatan dan kelurahan isi")
+            } 
+        });
+    }
+}
+
+
+function loadVotingChart(a, b, c, d, e, f, g, h, i, j, k, l) {
+    const apexChart = "#votingChartId";
+    var options = {
+        // series: [44, 55, 13, 43, 22, 10, 50, 34, 25, 30, 35, 20],
+        series: [a, b, c, d, e, f, g, h, i, j, k, l],
+        chart: {
+            width: 500,
+            type: 'pie',
+        },
+        legend: {
+            position: 'right',
+
+        },
+        noData: {
+            text: "No Data",
+            align: "center",
+            verticalAlign: "middle",
+        },
+        responsive: [{
+            breakpoint: 350,
+            options: {
+                chart: {
+                    width: 250
+                },
+                // legend: false,
+                legend: {
+                    position: 'right'
+                }
+            }
+        }],
+        colors: [primary, success, warning, danger, info],
+        stroke: {
+            width: 0
+        },
+        dataLabels: {
+            dropShadow: true,
+            style: {
+                fontSize: '20px',
+                fontWeight: 'bold',
+            },
+            background: {
+                enabled: true,
+                foreColor: 'black',
+                opacity: 1,
+                padding: 10,
+                borderRadius: 5,
+                borderWidth: 0,
+                borderColor: 'black'
+            },
+            offsetX: 0,
+            offsetY: 100,
+        },
+        labels: ['Balaraja', 'Cikupa', 'Cisauk', 'Cisoka', 'Curug', 'Jambe', 'Jayanti', 'Kelapa Dua', 'Kemiri', 'Kresek', 'Krojo', 'Kosambi'],
+        plotOptions: {
+        pie: {
+            startAngle: 0,
+            endAngle: 360,
+            expandOnClick: true,
+            offsetX: 0,
+            offsetY: 0,
+            customScale: 1,
+            dataLabels: {
+                offset: 0,
+                minAngleToShowLabel: 10
+            }, 
+            donut: {
+              size: '65%',
+              background: 'transparent',
+              labels: {
+                show: true,
+                name: {
+                  show: true,
+                  fontSize: '22px',
+                  fontFamily: 'Helvetica, Arial, sans-serif',
+                  fontWeight: 600,
+                  color: "#373d3f",
+                  offsetY: -10,
+                  formatter: function (val) {
+                    return val
+                  }
+                },
+                value: {
+                  show: true,
+                  fontSize: '18px',
+                  fontFamily: 'Helvetica, Arial, sans-serif',
+                  fontWeight: 500,
+                  color: undefined,
+                  offsetY: 16,
+                  formatter: function (val) {
+                    return val
+                  }
+                },
+                total: {
+                  show: true,
+                  showAlways: false,
+                  label: 'Total',
+                  fontSize: '22px',
+                  fontFamily: 'Helvetica, Arial, sans-serif',
+                  fontWeight: 600,
+                  color: '#373d3f',
+                  formatter: function (w) {
+                    return w.globals.seriesTotals.reduce((a, b) => {
+                      return a + b
+                    }, 0)
+                  }
+                }
+              }
+            },      
+          }
+        }
+        // legend: {
+            // formatter: function(seriesName, opts) {
+                // console.log(opts.w.globals.seriesTotals[opts.seriesIndex])
+                // var sum = 0
+                // sum = opts.w.globals.seriesTotals;
+
+                // console.log(sum.reduce((a, b) => a + b, 0))
+
+                // var sum = 0;
+                // for (i = 0; i<= opts.w.globals.series; i++) {
+                //     sum += opts.w.globals.series[i];
+                // }
+                // console.log(sum);
+                // return [seriesName, ": ", opts.w.globals.seriesTotals[opts.seriesIndex]]
+            // }
+        // }
+    };
+
+    document.getElementById("doChangeVoting").innerHTML = '<div id="votingChartId"></div>';
+
+
+    var chart = new ApexCharts(document.querySelector(apexChart), options);
+    chart.render();
+}
+
+function loadKtpKec() {
+    const apexChart = "#ktpKecChartId";
+    var options = {
+            series: [{
+                name: 'KTP Pemilih',
+                type: 'column',
+                data: [500, 300, 200, 300, 200, 280, 650, 460],
+                color: primary
+            }, {
+                name: 'KTP Terkumpul',
+                type: 'line',
+                data: [200, 300, 100, 150, 200, 220, 380, 35],
+                color: success
+            }, {
+                name: 'Target',
+                type: 'line',
+                data: [100, 200, 100, 200, 200, 110, 500, 400],
+                color: danger
+            }],
+            chart: {
+                height: 350,
+                type: 'line',
+                stacked: false
+            },
+            noData: {
+                text: "No Data",
+                align: "center",
+                verticalAlign: "middle",
+            },
+            dataLabels: {
+                enabled: false,
+                enabledOnSeries: [1]
+            },
+            stroke: {
+                width: [1, 4, 4]
+            },
+            markers: {
+              size: [4, 7]
+            },
+            // title: {
+            //     text: 'XYZ - Stock Analysis (2009 - 2016)',
+            //     align: 'left',
+            //     offsetX: 110
+            // },
+            xaxis: {
+                categories: ['Balaraja', 'Cikupa', 'Cisauk', 'Cisoka', 'Curug', 'Jambe', 'Jayanti', 'Kelapa Dua'],
+            },
+            yaxis: [
+                {
+                    axisTicks: {
+                        show: true,
+                    },
+                    // axisBorder: {
+                    //     show: true,
+                    //     color: primary
+                    // },
+                    // labels: {
+                    //     style: {
+                    //         colors: primary,
+                    //     }
+                    // },
+                    // title: {
+                    //     text: "KTP Terkumpul",
+                    //     style: {
+                    //         color: primary,
+                    //     }
+                    // },
+                    tooltip: {
+                        enabled: true
+                    }
+                },
+                
+            ],
+            tooltip: {
+                fixed: {
+                    enabled: true,
+                    position: 'topLeft', // topRight, topLeft, bottomRight, bottomLeft
+                    offsetY: 30,
+                    offsetX: 60
+                },
+                x: {
+                // formatter: function (x) {
+                //     if(typeof x !== "undefined") {
+                //         return "Kecmatan: " +  x.toFixed(0) ;
+                //     }
+                //     return x;
+
+                // }
+            }
+            },
+            legend: {
+                horizontalAlign: 'center',
+                offsetX: 40,
+                formatter: function(seriesName, opts) {
+                    // console.log(opts)
+
+                    // var sum = 0;
+                    // for (i = 0; i<= opts.w.globals.series; i++) {
+                    //     sum += opts.w.globals.series[i];
+                    // }
+                    // console.log(sum);
+                    return [seriesName, ": ", opts.w.globals.seriesTotals[opts.seriesIndex]]
+                }
+            }
+        };
+
+        document.getElementById("doChangeKtpKec").innerHTML = '<div id="ktpKecChartId"></div>';
+
+        var chart = new ApexCharts(document.querySelector(apexChart), options);
+        chart.render();
+}
+
+// function loadKtpKel() {
+//     const apexChart = "#ktpKelChartId";
+//     var options = {
+//             series: [{
+//                 name: 'KTP Pemilih',
+//                 type: 'column',
+//                 data: [500, 300, 200, 300, 200, 280, 650, 460],
+//                 color: primary
+//             }, {
+//                 name: 'KTP Terkumpul',
+//                 type: 'line',
+//                 data: [200, 300, 100, 150, 200, 220, 380, 35],
+//                 color: success
+//             }, {
+//                 name: 'Target',
+//                 type: 'line',
+//                 data: [100, 200, 100, 200, 200, 110, 500, 400],
+//                 color: danger
+//             }],
+//             chart: {
+//                 height: 350,
+//                 type: 'line',
+//                 stacked: false
+//             },
+//             noData: {
+//                 text: "No Data",
+//                 align: "center",
+//                 verticalAlign: "middle",
+//             },
+//             dataLabels: {
+//                 enabled: false,
+//                 enabledOnSeries: [1]
+//             },
+//             stroke: {
+//                 width: [1, 4, 4]
+//             },
+//             markers: {
+//               size: [4, 7]
+//             },
+//             // title: {
+//             //     text: 'XYZ - Stock Analysis (2009 - 2016)',
+//             //     align: 'left',
+//             //     offsetX: 110
+//             // },
+//             xaxis: {
+//                 categories: ['Balaraja', 'Cikupa', 'Cisauk', 'Cisoka', 'Curug', 'Jambe', 'Jayanti', 'Kelapa Dua'],
+//             },
+//             yaxis: [
+//                 {
+//                     axisTicks: {
+//                         show: true,
+//                     },
+//                     // axisBorder: {
+//                     //     show: true,
+//                     //     color: primary
+//                     // },
+//                     // labels: {
+//                     //     style: {
+//                     //         colors: primary,
+//                     //     }
+//                     // },
+//                     // title: {
+//                     //     text: "KTP Terkumpul",
+//                     //     style: {
+//                     //         color: primary,
+//                     //     }
+//                     // },
+//                     tooltip: {
+//                         enabled: true
+//                     }
+//                 },
+                
+//             ],
+//             tooltip: {
+//                 fixed: {
+//                     enabled: true,
+//                     position: 'topLeft', // topRight, topLeft, bottomRight, bottomLeft
+//                     offsetY: 30,
+//                     offsetX: 60
+//                 },
+//                 x: {
+//                 // formatter: function (x) {
+//                 //     if(typeof x !== "undefined") {
+//                 //         return "Kecmatan: " +  x.toFixed(0) ;
+//                 //     }
+//                 //     return x;
+
+//                 // }
+//             }
+//             },
+//             legend: {
+//                 horizontalAlign: 'left',
+//                 offsetX: 40
+//             }
+//         };
+
+//         document.getElementById("doChangeKtpKel").innerHTML = '<div id="ktpKelChartId"></div>';
+
+//         var chart = new ApexCharts(document.querySelector(apexChart), options);
+//         chart.render();
+// }
+
---
+++ assets/js/app/ktp.js
@@ -0,0 +1,213 @@
+var table, genderSelector, religionSelector, maritalSelector, nationalitySelector
+
+function Ktp() {
+	var self = this;
+
+	self.init = function() {
+
+		var avatar1 = new KTImageInput('kt_image_1');
+
+		var fileSelector = document.querySelector('#imgInput');
+
+		// if (fileSelector != null) {
+			
+		// }
+
+		
+
+		table = $('#dt-ktps').DataTable({
+			'dom': 'rtip',
+            'pageLength': 20,
+            'responsive': true,
+			'columnDefs': [
+				{
+					targets: 0,
+					render: function(data, type, full, meta) {
+
+						var output;
+						var stateNo = KTUtil.getRandomInt(0, 7);
+						var states = [
+							'success',
+							'light',
+							'danger',
+							'success',
+							'warning',
+							'dark',
+							'primary',
+							'info'];
+
+						var state = states[stateNo];
+
+						output = `
+                            <div class="d-flex align-items-center">
+                                <div class="symbol symbol-50 symbol-light-` + state + `" flex-shrink-0">
+                                    <div class="symbol-label font-size-h5">` + full[0].substring(0, 1) + `</div>
+                                </div>
+                                <div class="ml-3">
+                                    <span class="text-dark-75 font-weight-bold line-height-sm d-block pb-2">` + full[0] + `</span>
+                                </div>
+                            </div>`;
+
+						return output;
+					},
+				},
+				// {
+				// 	targets: 1,
+				// 	render: function(data, type, full, meta) {
+				// 		return '<a class="text-dark-50 text-hover-primary" href="mailto:' + data + '">' + data + '</a>';
+				// 	},
+				// },
+				{
+					targets: -1,
+					title: 'Actions',
+					orderable: false,
+					render: function(data, type, full, meta) {
+						return '\
+							<div class="dropdown dropdown-inline">\
+								<a href="javascript:;" class="btn btn-sm btn-clean btn-icon" data-toggle="dropdown">\
+	                                <i class="la la-cog"></i>\
+	                            </a>\
+							  	<div class="dropdown-menu dropdown-menu-sm dropdown-menu-right">\
+									<ul class="nav nav-hoverable flex-column">\
+							    		<li class="nav-item"><a class="nav-link" href="#"><i class="nav-icon la la-edit"></i><span class="nav-text">Edit Details</span></a></li>\
+							    		<li class="nav-item"><a class="nav-link" href="#"><i class="nav-icon la la-leaf"></i><span class="nav-text">Update Status</span></a></li>\
+							    		<li class="nav-item"><a class="nav-link" href="#"><i class="nav-icon la la-print"></i><span class="nav-text">Print</span></a></li>\
+									</ul>\
+							  	</div>\
+							</div>\
+							<a href="javascript:;" class="btn btn-sm btn-clean btn-icon" title="Edit details">\
+								<i class="la la-edit"></i>\
+							</a>\
+							<a href="javascript:;" class="btn btn-sm btn-clean btn-icon" title="Delete">\
+								<i class="la la-trash"></i>\
+							</a>\
+						';
+					},
+				},
+				{
+					targets: 5,
+					render: function(data, type, full, meta) {
+						var status = {
+							1: {'title': 'ACTIVE', 'class': ' label-light-success'},
+							2: {'title': 'INACTIVE', 'class': ' label-light-danger'},
+						};
+						if (typeof status[data] === 'undefined') {
+							return data;
+						}
+						return '<span class="label label-lg font-weight-bold' + status[data].class + ' label-inline">' + status[data].title + '</span>';
+					},
+				},
+				{
+					targets: 6,
+					render: function(data, type, full, meta) {
+						return '<span class="text-muted">' + data + '</span>';
+					},
+				},
+				{
+					targets: 7,
+					render: function(data, type, full, meta) {
+						return '<span class="text-muted">' + data + '</span>';
+					},
+				},
+			],
+		});
+
+		genderSelector = $('#dataKtpForm select[name="genderId"]').select2({
+            width: '100%',
+            placeholder: "Silahkan pilih jenis kelamin",
+            dropdownParent: $("#dataKtpForm")
+        });
+
+        religionSelector = $('#dataKtpForm select[name="religionId"]').select2({
+            width: '100%',
+            placeholder: "Silahkan pilih agama",
+            dropdownParent: $("#dataKtpForm")
+        });
+
+        maritalSelector = $('#dataKtpForm select[name="maritalId"]').select2({
+            width: '100%',
+            placeholder: "Silahkan pilih perkawinan",
+            dropdownParent: $("#dataKtpForm")
+        });
+
+        nationalitySelector = $('#dataKtpForm select[name="nationalityId"]').select2({
+            width: '100%',
+            placeholder: "Silahkan pilih kewarganegaraan",
+            dropdownParent: $("#dataKtpForm")
+        });
+
+
+		$('#btnAdd').on('click', function(e) {
+			e.preventDefault();
+
+			$('#dataKtpModal').modal({
+				backdrop: 'static',
+				keyboard: true,
+				show: true
+			});
+		});
+
+		$('input[type=file]').on('change', function(e) {
+			e.preventDefault();
+
+			var nik = ''
+			var name = ''
+			var gender = ''
+			var religion = ''
+			var marital = ''
+			var nationality = ''
+
+
+			// console.log(fileSelector)
+
+
+			// var rec = new Tesseract.TesseractWorker();
+			// // rec.recognize(fileSelector.files[0])
+			// // 	.progress(function (response) {
+			// // 		console.log(response)
+			// // 	})
+			// // 	.then(function (data) {
+			// // 		console.log(data)
+			// // 	})
+
+			// const { data: { text } } = await worker.recognize(fileSelector.files[0]);
+			// console.log(data)
+
+			const { createWorker } = Tesseract;
+
+			(async () => {
+			  const worker = await createWorker("ind", 1, {
+			  	logger: m => console.log(m),
+			  });
+			  const { data: { lines } } = await worker.recognize(fileSelector.files[0], {
+			  	rotateAuto: true
+			  }, {imageColor: true, imageGrey: true, imageBinary: true});
+			  // const data = await worker.recognize(fileSelector.files[0]);
+			  console.log(lines)
+
+			  // #nik
+			  document.getElementById("ktp").setAttribute('value', lines[2].words[2].text)
+			  console.log(lines[2].words[2].text);
+
+			  // name
+			  var result = '';
+			  for(var i=2; i<lines[3].words.length; i++) {
+			  	result += lines[3].words[i].text + ' '
+			  }
+			  console.log(result)
+			  document.getElementById("name").setAttribute('value', result)
+
+			  // gender
+			  console.log(lines[5].words[3].text)
+			  $("#genderId").val(lines[5].words[3].text).trigger('change');
+
+			  // religion
+			  console.log(lines[10].words[2].text)
+			  $("#religionId").val(lines[10].words[2].text).trigger('change');
+
+			  await worker.terminate();
+			})();
+		});
+
+	}
+}	
